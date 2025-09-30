c0fd53ffbc38c10a3fb2e48c344569eb6ded892e
Hustandant <84928386+Hustoroff@users.noreply.github.com>
2025-10-01 01:01:56 +0300

    Update Fullscreen piano.js
    Fix for defining parent element for button placement

--- Fullscreen piano.js
+++ Fullscreen piano.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         Fullscreen piano
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Script to increase the size of the piano by hiding the chat window
 // @author       Hustandant
 // @match        *://pianoverse.net/*
 // @icon         https://avatars.githubusercontent.com/u/84928386
 // @grant        none
-// @license      MIT
 // ==/UserScript==
 
 (function() {
@@ -15,12 +14,12 @@
     window.addEventListener('load', () => {
         const openChat = document.createElement("div");
         const chat = document.querySelector(".chat");
-        const toolbar = document.querySelector(".piano pv-toolbar .left");
+        const toolbar = document.querySelector(".piano pv-toolbar .block");
         const piano = document.querySelector(".piano");
 
         piano.style.width = "100%";
 
-        openChat.classList.add("open-chat");
+        openChat.classList.add("open-chat", "toolbar-button");
         openChat.style.height = "100%"
 
         toolbar.prepend(openChat);
